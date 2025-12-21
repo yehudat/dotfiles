cb86a6844fcc33fa7ae62fbd8dba5367135f06f8
Yehuda Tsimanis <yehuda.tsimanis@gmail.com>
2025-12-22 00:11:22 +0200

    [chezmoi] [tmux] cfg tmux and nvim integration

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -170,4 +170,22 @@ return {
   -- Airline theme replacement - handled by NvChad statusline
 
   -- Devicons - already in NvChad
+
+  -- Tmux integration (seamless pane navigation)
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+    },
+    keys = {
+      { "<C-h>", "<cmd>TmuxNavigateLeft<CR>" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<CR>" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<CR>" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<CR>" },
+    },
+  },
 }
